f5a1b8e65a28cb1bbe468b2a06f3c94e0d84a1ac
Francesco Romani <fromani@gmail.com>
2010-02-14 11:37:29 +0100

    slightly better usage of the unittest API.

--- tests/packettest.py
+++ tests/packettest.py
@@ -3,7 +3,7 @@
 import pyrana
 import unittest
 
-class FrameTestCase(unittest.TestCase):
+class PacketTestCase(unittest.TestCase):
     def test_NewFromString(self):
         try:
             f = pyrana.format.Packet(0, "a")
@@ -14,7 +14,7 @@ class FrameTestCase(unittest.TestCase):
             f = pyrana.format.Packet(0, "a" * 1024 * 1024 * 32)
         except:
             self.fail("failed creation from huge string")
-    def test_NewFromFrame(self):
+    def test_NewFromPacket(self):
         try:
             f = pyrana.format.Packet(0, "a")
             g = pyrana.format.Packet(1, f)
@@ -22,53 +22,53 @@ class FrameTestCase(unittest.TestCase):
             self.fail("failed creation from another packet")
     def test_DataValuesMatches(self):
         f = pyrana.format.Packet(0, "a")
-        assert(f.data == "a")
-        assert(f.size == 1)
+        self.assertTrue(f.data == "a")
+        self.assertTrue(f.size == 1)
     def test_DefaultValues(self):
         f = pyrana.format.Packet(0, "a")
-        assert(not f.isKey)
-        assert(f.pts == pyrana.TS_NULL)
-        assert(f.dts == pyrana.TS_NULL)
-        assert(f.idx  == 0)
+        self.assertFalse(f.isKey)
+        self.assertTrue(f.pts == pyrana.TS_NULL)
+        self.assertTrue(f.dts == pyrana.TS_NULL)
+        self.assertTrue(f.idx  == 0)
     def test_InitValues(self):
         f = pyrana.format.Packet(3, "abracadabra", 200, 300, True)
-        assert(f.idx == 3)
-        assert(f.isKey)
-        assert(f.pts == 200)
-        assert(f.dts == 300)
+        self.assertTrue(f.idx == 3)
+        self.assertTrue(f.isKey)
+        self.assertTrue(f.pts == 200)
+        self.assertTrue(f.dts == 300)
     def test_CannotResetStreamIndex(self):
         f = pyrana.format.Packet(0, "gammaray")
-        assert(f.idx == 0)
+        self.assertTrue(f.idx == 0)
         self.assertRaises(AttributeError, setattr, f, "idx", 23)
-        assert(f.idx == 0)
+        self.assertTrue(f.idx == 0)
     def test_CannotResetIsKey(self):
         f = pyrana.format.Packet(0, "gammaray")
-        assert(f.isKey == 0)
+        self.assertFalse(f.isKey)
         self.assertRaises(AttributeError, setattr, f, "isKey", 1)
-        assert(f.isKey == 0)
+        self.assertFalse(f.isKey)
     def test_CannotResetPTS(self):
         f = pyrana.format.Packet(0, "R'Yleh")
-        assert(f.pts == pyrana.TS_NULL)
+        self.assertTrue(f.pts == pyrana.TS_NULL)
         self.assertRaises(AttributeError, setattr, f, "pts", 42)
-        assert(f.pts == pyrana.TS_NULL)
+        self.assertTrue(f.pts == pyrana.TS_NULL)
     def test_CannotResetDTS(self):
         f = pyrana.format.Packet(0, "R'Yleh")
-        assert(f.dts == pyrana.TS_NULL)
+        self.assertTrue(f.dts == pyrana.TS_NULL)
         self.assertRaises(AttributeError, setattr, f, "dts", 42)
-        assert(f.dts == pyrana.TS_NULL)
+        self.assertTrue(f.dts == pyrana.TS_NULL)
     def test_CannotResetData(self):
         d = "O RLY?!?"
         f = pyrana.format.Packet(0, d)
-        assert(f.data == d)
+        self.assertTrue(f.data == d)
         self.assertRaises(AttributeError, setattr, f, "data", "YA, RLY")
-        assert(f.data == d)
+        self.assertTrue(f.data == d)
     def test_CannotResetSize(self):
         d = "YA, RLY!!!"
         l = len(d)
         f = pyrana.format.Packet(0, d)
-        assert(f.size == l)
+        self.assertTrue(f.size == l)
         self.assertRaises(AttributeError, setattr, f, "size", len("YA, RLY"))
-        assert(f.size == l)
+        self.assertTrue(f.size == l)
 
 
 
