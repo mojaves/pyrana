2e01b212cbee981fe478da7c5da280ec111e8dc1
Francesco Romani <fromani@gmail.com>
2010-02-14 21:26:13 +0100

    take in account iterator protocol for demuxer stress test.

--- tests/demuxerstresstest.py
+++ tests/demuxerstresstest.py
@@ -9,19 +9,41 @@ if len(args) != 1:
     sys.stderr.write("usage: %s filename\n" %(sys.argv[0]))
     sys.exit(1)
 
-f = open(args[0], "rb")
-dmx = pyrana.format.Demuxer(f)
+def stressWhile(name):
+    f = open(name, "rb")
+    dmx = pyrana.format.Demuxer(f)
 
-frames = 0
-hasData = True
-while hasData:
-    try:
-        Fr = dmx.readFrame()
-    except pyrana.EOSError:
-        sys.stdout.write("\nmaster stream ends here!\n")
-        hasData = False
-    sys.stdout.write("elapsed frames: %i\r" %(frames))
-    frames += 1
+    sys.stdout.write("stressWhile()\n")
+    frames = 0
+    hasData = True
+    while hasData:
+        try:
+            Fr = dmx.readFrame()
+        except pyrana.EOSError:
+            sys.stdout.write("\nmaster stream ends here!\n")
+            hasData = False
+        print Fr
+        sys.stdout.write("elapsed frames: %i\r" %(frames))
+        frames += 1
+    sys.stdout.write("\n")
+    f.close()
 
-sys.stdout.write("\n")
+def stressIter(name):
+    f = open(name, "rb")
+    dmx = pyrana.format.Demuxer(f)
+
+    sys.stdout.write("stressIter()\n")
+    frames = 0
+    for Fr in dmx:
+        print Fr
+        sys.stdout.write("elapsed frames: %i\r" %(frames))
+        frames += 1
+    sys.stdout.write("\nmaster stream ends here!\n")
+    sys.stdout.write("\n")
+    f.close()
+
+
+
+stressWhile(args[0])
+stressIter(args[0])
 
