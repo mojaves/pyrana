a1fcc1abd90e9a8ec3602e80d22fba39b048ccf0
Francesco Romani <fromani@gmail.com>
2013-08-03 15:44:48 +0200

    port to python 3
    no more string vs bytes clash.
    Needs more polishing, still.
    
    --HG--
    branch : pure-cffi

--- tests/test_packet.py
+++ tests/test_packet.py
@@ -1,58 +1,61 @@
 #!/usr/bin/python
 
 import pyrana
+import pyrana.format
 import unittest
+
+_B = "a".encode('utf-8')
 
 class TestPacket(unittest.TestCase):
     def test_NewFromString(self):
         try:
-            f = pyrana.format.Packet(0, "a")
+            f = pyrana.format.Packet(0, _B)
         except pyrana.PyranaError as x:
             self.fail("failed creation from simple string: %s" %str(x))
     def test_NewFromStringHuge(self):
         try:
-            f = pyrana.format.Packet(0, "a" * 1024 * 1024 * 32)
+            f = pyrana.format.Packet(0, _B * 1024 * 1024 * 32)
         except:
             self.fail("failed creation from huge string")
     def test_NewFromPacket(self):
         try:
-            f = pyrana.format.Packet(0, "a")
+            f = pyrana.format.Packet(0, _B)
             g = pyrana.format.Packet(1, f)
         except pyrana.PyranaError as x:
             self.fail("failed creation from another packet")
     def test_DataValuesMatches(self):
-        f = pyrana.format.Packet(0, "a")
+        f = pyrana.format.Packet(0, _B)
         self.assertTrue(f.data == b"a")
         self.assertTrue(f.size == 1)
     def test_DefaultValues(self):
-        f = pyrana.format.Packet(0, "a")
+        f = pyrana.format.Packet(0, _B)
         self.assertFalse(f.is_key)
         self.assertTrue(f.pts == pyrana.TS_NULL)
         self.assertTrue(f.dts == pyrana.TS_NULL)
         self.assertTrue(f.stream_id  == 0)
     def test_InitValues(self):
-        f = pyrana.format.Packet(3, "abracadabra", 200, 300, True)
+        f = pyrana.format.Packet(3, "abracadabra".encode('utf-8'), 200, 300, True)
         self.assertTrue(f.stream_id == 3)
         self.assertTrue(f.is_key)
         self.assertTrue(f.pts == 200)
         self.assertTrue(f.dts == 300)
     def test_CannotResetStreamIndex(self):
-        f = pyrana.format.Packet(0, "gammaray")
+        f = pyrana.format.Packet(0, "gammaray".encode('utf-8'))
         self.assertTrue(f.stream_id == 0)
         self.assertRaises(AttributeError, setattr, f, "stream_id", 23)
         self.assertTrue(f.stream_id == 0)
     def test_CannotResetIsKey(self):
-        f = pyrana.format.Packet(0, "gammaray")
+        f = pyrana.format.Packet(0, "gammaray".encode('utf-8'))
         self.assertFalse(f.is_key)
         self.assertRaises(AttributeError, setattr, f, "is_key", 1)
         self.assertFalse(f.is_key)
     def test_CannotResetPTS(self):
-        f = pyrana.format.Packet(0, "R'Yleh")
+        f = pyrana.format.Packet(0, "R'Yleh".encode('utf-8'))
         self.assertTrue(f.pts == pyrana.TS_NULL)
         self.assertRaises(AttributeError, setattr, f, "pts", 42)
         self.assertTrue(f.pts == pyrana.TS_NULL)
     def test_CannotResetDTS(self):
-        f = pyrana.format.Packet(0, "R'Yleh")
+        f = pyrana.format.Packet(0, "R'Yleh".encode('utf-8'))
         self.assertTrue(f.dts == pyrana.TS_NULL)
         self.assertRaises(AttributeError, setattr, f, "dts", 42)
         self.assertTrue(f.dts == pyrana.TS_NULL)
