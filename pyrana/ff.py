88d34e207dd1e92279da809a1af5c05bc3f77066
Francesco Romani <fromani@gmail.com>
2013-08-15 18:42:32 +0200

    we shouls have exactly one FFI instance.
    also, add more fields in AVFormatContext.
    
    --HG--
    branch : pure-cffi

--- pyrana/ff.py
+++ pyrana/ff.py
@@ -27,6 +27,7 @@ def _wire(ffi):
              AVOutputFormat *av_oformat_next(AVOutputFormat *F);
 
              void *av_malloc(size_t size);
+             void *av_mallocz(size_t size);
              void av_free(void *ptr);
 
              typedef struct AVIOContext AVIOContext;
@@ -39,17 +40,46 @@ def _wire(ffi):
                     int (*write_packet)(void *opaque, uint8_t *buf, int buf_size),
                     int64_t (*seek)(void *opaque, int64_t offset, int whence));
 
+
+             typedef struct AVDictionary AVDictionary;
+             typedef struct AVClass AVClass;
+             typedef struct AVProgram AVProgram;
+             typedef struct AVChapter AVChapter;
+             typedef struct AVStream AVStream;
+
              typedef struct AVFormatContext {
-                 const void *av_class;
+                 const AVClass *av_class;
                  struct AVInputFormat *iformat;
                  struct AVOutputFormat *oformat;
                  void *priv_data;
                  AVIOContext *pb;
                  int ctx_flags;
                  unsigned int nb_streams;
-                 void **streams;
+                 AVStream **streams;
                  char filename[1024];
                  int64_t start_time;
+                 int64_t duration;
+                 int bit_rate;
+                 unsigned int packet_size;
+                 int max_delay;
+                 int flags;
+                 unsigned int probesize;
+                 int max_analyze_duration;
+                 const uint8_t *key;
+                 int keylen;
+                 unsigned int nb_programs;
+                 AVProgram **programs;
+                 enum AVCodecID video_codec_id;
+                 enum AVCodecID audio_codec_id;
+                 enum AVCodecID subtitle_codec_id;
+                 unsigned int max_index_size;
+                 unsigned int max_picture_buffer;
+                 unsigned int nb_chapters;
+                 AVChapter **chapters;
+                 AVDictionary *metadata;
+                 int64_t start_time_realtime;
+                 int fps_probe_size;
+                 int error_recognition;
                  /* ... */
              } AVFormatContext;
              AVFormatContext *avformat_alloc_context(void);
@@ -59,21 +89,32 @@ def _wire(ffi):
              """)
 
 
-# this is a Borg.
-# http://code.activestate.com/recipes/66531-singleton-we-dont-need-no-stinkin-singleton-the-bo/
+# The dreaded singleton. It is a necessary evil[1] and this is the reason why:
+# https://bitbucket.org/cffi/cffi/issue/4/typeerror-initializer-for-ctype-double
+#
+# FIXME: need a more pythonic way to enforce unique-ness.
+# what we need yet is a 
+
 class FF:
-    __shared_state = {}
+    __inited = False
+    __instance = None
     def __new__(cls, *args, **kwargs):
-        instance = super().__new__(cls, *args, **kwargs)
-        instance.__dict__ = cls.__shared_state
-        return instance
+        if FF.__instance is None:
+            FF.__instance = super().__new__(cls, *args, **kwargs)
+        return FF.__instance
     def __init__(self):
-        self.ffi = cffi.FFI()
-        _wire(self.ffi)
-        self.lavc = self.ffi.dlopen("avcodec")
-        self.lavf = self.ffi.dlopen("avformat")
-        self.lavu = self.ffi.dlopen("avutil")
+        if not FF.__inited:
+            self.ffi = cffi.FFI()
+            _wire(self.ffi)
+            self.lavc = self.ffi.dlopen("avcodec")
+            self.lavf = self.ffi.dlopen("avformat")
+            self.lavu = self.ffi.dlopen("avutil")
+            FF.__inited = True
     def versions(self):
         return (self.lavc.avcodec_version(),
                 self.lavf.avformat_version(),
                 self.lavu.avutil_version())
+
+def getFF():
+    return FF()
+
