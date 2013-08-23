9203ae479ff5b7e3d5e0776fd67f96c3e717c896
Francesco Romani <fromani@gmail.com>
2013-08-23 08:24:46 +0200

    first stub of format._read() test
    --HG--
    branch : pure-cffi

--- tests/test_format_iosource.py
+++ tests/test_format_iosource.py
@@ -5,25 +5,39 @@ import io
 import unittest
 
 
-_B = b'\0' * 1024 * 64
+_BZ = b'\0' * 1024 * 64
 
 
 class TestFormatIOSource(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        pyrana.setup()
+
     def test_new_empty(self):
-        f = io.BytesIO(_B)
+        f = io.BytesIO(_BZ)
         src = pyrana.format.IOSource(f)
         assert src
 
     def test_new_empty_not_seekable(self):
-        f = io.BytesIO(_B)
+        f = io.BytesIO(_BZ)
         src = pyrana.format.IOSource(f, seekable=False)
         assert src
 
     def test_new_empty_custom_size(self):
-        f = io.BytesIO(_B)
+        f = io.BytesIO(_BZ)
         size = pyrana.format.PKT_SIZE * 4
         src = pyrana.format.IOSource(f, bufsize=size)
         assert src
+
+    def test_read(self):
+        ffh = pyrana.ff.get_handle()
+        buf = pyrana.format.Buffer()
+        f = io.BytesIO(_BZ)
+        h = ffh.ffi.new_handle(f)
+        pyrana.format._read(h, buf.cdata, buf.size)
+        for b in buf.data:
+            assert(b == b'\x00')
+
 
 if __name__ == "__main__":
     unittest.main()
