88d6c15dbfdb9e043c70350dadbad61d90dfabf2
Francesco Romani <fromani@gmail.com>
2013-08-24 12:04:46 +0200

    fix read and seek test
    and we don't need yet the write function.
    
    --HG--
    branch : pure-cffi

--- tests/test_format_iosource.py
+++ tests/test_format_iosource.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python
 
-import pyrana.format
+import random
 import io
 import unittest
+import pyrana.format
 
 
-_BZ = b'\0' * 1024 * 64
+_BLEN = 64 * 1024
+_BZ = b'\0' * _BLEN
+
+
+def _randgen(L, x=None):
+    # FIXME: clumsy, rewrite
+    cnt, num = 0, L
+    rnd = random.Random(x)
+    while cnt < L:
+        yield (rnd.randint(0, 255),)
+        cnt += 1
 
 
 class TestFormatIOSource(unittest.TestCase):
@@ -35,8 +46,36 @@ class TestFormatIOSource(unittest.TestCase):
         f = io.BytesIO(_BZ)
         h = ffh.ffi.new_handle(f)
         pyrana.format._read(h, buf.cdata, buf.size)
-        for b in buf.data:
-            assert(b == b'\x00')
+        _x = f.getbuffer()
+        try:
+            _x = _x.cast('c')  # cpython >= 3.3
+        except:
+            # cpyhon 3.2: already returns bytes as items.
+            pass
+        for i, b in enumerate(buf.data):
+            assert(b == _x[i])  # XXX sigh. I can barely stand this.
+
+# not yet needed
+#    def test_write(self):
+#        ffh = pyrana.ff.get_handle()
+#        buf = pyrana.format.Buffer(_BLEN)
+#        for i, b in enumerate(_randgen(_BLEN)):
+#            buf.data[i] = bytes(b)  # XXX whoa,
+#                                    # that's almost too ugly to be true
+#        f = io.BytesIO()
+#        h = ffh.ffi.new_handle(f)
+#        pyrana.format._write(h, buf.cdata, buf.size)
+#        _x = f.getbuffer()
+#        for i, b in enumerate(buf.data):
+#            assert(b == bytes((_x[i],)))  # XXX you sure?
+
+    def test_seek(self):
+        ffh = pyrana.ff.get_handle()
+        buf = pyrana.format.Buffer()
+        f = io.BytesIO(_BZ)
+        h = ffh.ffi.new_handle(f)
+        pyrana.format._seek(h, 128, 0)
+        self.assertEquals(f.tell(), 128)
 
 
 if __name__ == "__main__":
