37feb4cae9d01b3b438d8a8adb5401b4213ed0bc
Francesco Romani <fromani@gmail.com>
2013-08-24 10:00:49 +0200

    iosource requires setup.
    --HG--
    branch : pure-cffi

--- tests/test_format_iosource.py
+++ tests/test_format_iosource.py
@@ -5,22 +5,26 @@ import io
 import unittest
 
 
-_B = b'\0' * 1024 * 64
+_BZ = b'\0' * 1024 * 64
 
 
 class TestFormatIOSource(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        pyrana.setup()
+
     def test_new_empty(self):
-        f = io.BytesIO(_B)
+        f = io.BytesIO(_BZ)
         src = pyrana.format.IOSource(f)
         assert src
 
     def test_new_empty_not_seekable(self):
-        f = io.BytesIO(_B)
+        f = io.BytesIO(_BZ)
         src = pyrana.format.IOSource(f, seekable=False)
         assert src
 
     def test_new_empty_custom_size(self):
-        f = io.BytesIO(_B)
+        f = io.BytesIO(_BZ)
         size = pyrana.format.PKT_SIZE * 4
         src = pyrana.format.IOSource(f, bufsize=size)
         assert src
