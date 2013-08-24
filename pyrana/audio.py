19183382fced3e54813f1c5cf2bb45f006a9c6a7
Francesco Romani <fromani@gmail.com>
2013-08-24 14:58:20 +0200

    docstrings.
    --HG--
    branch : pure-cffi

--- pyrana/audio.py
+++ pyrana/audio.py
@@ -1,3 +1,8 @@
+"""
+this module provides the audio codec interface.
+Encoders, Decoders and their support code.
+"""
+
 from pyrana.samplefmt import SampleFormat
 
 INPUT_CODECS = frozenset()
--- pyrana/video.py
+++ pyrana/video.py
@@ -1,3 +1,8 @@
+"""
+this module provides the video codec interface.
+Encoders, Decoders and their support code.
+"""
+
 from pyrana.pixelfmt import PixelFormat
 
 INPUT_CODECS = frozenset()
