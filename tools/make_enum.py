35dda5efb6ebd005861a70a35f08bcfc7a9262fa
Pippolo84 <fabio.falzoi84@gmail.com>
2013-08-27 22:49:13 +0200

    Fixed a bug in make_enum utility to parse nested enum included from other header files
    --HG--
    branch : pure-cffi

--- tools/make_enum.py
+++ tools/make_enum.py
@@ -10,6 +10,15 @@ import hashlib
 import sys
 
 
+class NoValueException(Exception):
+    """
+    Used when we are searching for a specific
+    enumerator value and we don't find an explicit
+    value associated with it
+    """
+    pass
+
+
 def compute_sha1_hash(file_to_hash):
     """
     Returns the SHA-1 hash of the file passed
@@ -26,7 +35,7 @@ class EnumTranslator(pycparser.c_ast.NodeVisitor):
     Defines objects able to translate a C enum type
     in a Python class.
     Takes an AST as input, representing the C enum type
-    and produces a corresponding Python class that 
+    and produces a corresponding Python class that
     inherits from IntEnum.
     To be able to visit the AST nodes, it inherits from
     NodeVisitor
@@ -42,9 +51,9 @@ class EnumTranslator(pycparser.c_ast.NodeVisitor):
     }
 
     def __init__(
-            self, 
-            abstract_syntax_tree, 
-            enum_name='', 
+            self,
+            abstract_syntax_tree,
+            enum_name='',
             output_class_name=''):
         self.ast = abstract_syntax_tree
         self.enum_name = enum_name
@@ -53,6 +62,27 @@ class EnumTranslator(pycparser.c_ast.NodeVisitor):
         else:
             self.output_class_name = output_class_name
         self.output_buffer = []
+
+    def _search_enum_value(self, node, name_to_search):
+        """
+        Searches for the enumerators name_to_search contained
+        in the portion of the AST identified by node
+        Returns the value associated with that name if found,
+        None otherwise
+        """
+        for elem in node.values.enumerators:
+            if elem.name == name_to_search:
+                if elem.value:
+                    return self._enumvalue(elem)
+                else:
+                    raise NoValueException("No associated value for "
+                                           "enumerator{}".format(elem.name))
+        # FIXME
+        """
+        It is possible that the enumerator is not expressed as NAME = VALUE
+        in the source header file, so we need to calculate the value
+        associated to the enumerator (like we do in _translate_enum)
+        """
 
     def _enumvalue(self, enum_token):
         """
@@ -65,20 +95,13 @@ class EnumTranslator(pycparser.c_ast.NodeVisitor):
         elif isinstance(enum_token.value, pycparser.c_ast.UnaryOp):
             val = enum_token.value.expr.value
             return self.__class__.UNARYOPS[enum_token.value.op](
-                    int(val, base=0))
+                int(val, base=0))
         elif isinstance(enum_token.value, pycparser.c_ast.BinaryOp):
             lval = enum_token.value.left.value
             rval = enum_token.value.right.value
             return self.__class__.BINARYOPS[enum_token.value.op](
                 int(lval, base=0),
                 int(rval, base=0))
-        #FIXME
-        """
-        else:
-            self.output_buffer.append(enum_token.name + '\n')
-            self.output_buffer.append(enum_token.value + '\n')
-            self.output_buffer.append(dir(enum_token.value) + '\n')
-        """
 
     def _translate_enum(self, node):
         """
@@ -90,21 +113,25 @@ class EnumTranslator(pycparser.c_ast.NodeVisitor):
         for elem in node.values.enumerators:
             if elem.value:
                 value = self._enumvalue(elem)
+                if value is None:
+                    for (childname, child) in elem.children():
+                        if isinstance(child, pycparser.c_ast.ID):
+                            #FIXME
+                            # This may lead to wrong enumerator value
+                            # calculation
+                            value = self._search_enum_value(node, child.name)
                 count = value
             else:
                 value = count+1
                 count += 1
             self.output_buffer.append(
-                    '    {0} = {1}\n'.format(elem.name, value))
-            #FIXME
-            if elem.name == 'AV_PIX_FMT_NB':
-                break
+                '    {0} = {1}\n'.format(elem.name, value))
 
     def visit_Enum(self, node):
         """
         This method is called for each Enun node
         found while visiting the AST.
-        For further detail please refer to 
+        For further detail please refer to
         pycparser/c_ast.py line 81
         """
         if not self.enum_name or node.name == self.enum_name:
@@ -129,7 +156,8 @@ class EnumTranslator(pycparser.c_ast.NodeVisitor):
         if import_line:
             self.output_buffer.append('from enum import IntEnum\n')
             self.output_buffer.append('\n')
-        self.output_buffer.append('class {}(IntEnum):\n'.format(self.enum_name))
+        self.output_buffer.append('class {}(IntEnum)'
+                                  ':\n'.format(self.enum_name))
         self.output_buffer.append('    """\n')
         if comment:
             self.output_buffer.append('    {}\n'.format(comment))
@@ -163,9 +191,10 @@ class EnumTranslator(pycparser.c_ast.NodeVisitor):
         """
         out_stream.write(''.join(self.output_buffer))
 
+
 def _main():
     """
-    Manage command line arguments and 
+    Manage command line arguments and
     creates an EnumTranslator
     to obtain a Python class
     """
@@ -200,11 +229,11 @@ def _main():
 
     et_obj = EnumTranslator(ast, args_obj.enum, args_obj.output_class)
     translate_params = {
-            'import_line': args_obj.importline,
-            'header': args_obj.header,
-            'comment': args_obj.comment,
-            'hash_type': 'SHA-1',
-            'hash_value': compute_sha1_hash(args_obj.header)
+        'import_line': args_obj.importline,
+        'header': args_obj.header,
+        'comment': args_obj.comment,
+        'hash_type': 'SHA-1',
+        'hash_value': compute_sha1_hash(args_obj.header)
     }
     et_obj.translate(translate_params)
 
