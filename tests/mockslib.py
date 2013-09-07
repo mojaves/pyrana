3207f675bbc1d89642cdb3f5503dd362a378df13
Francesco Romani <fromani@gmail.com>
2013-09-07 18:44:54 +0200

    clean the mocks collection

--- tests/mockslib.py
+++ tests/mockslib.py
@@ -4,7 +4,20 @@
 
 
 class MockLavc:
-    def av_new_packet(self, pkt, size):
+    @staticmethod
+    def av_new_packet(pkt, size):
+        return -1
+
+    @staticmethod
+    def avcodec_find_decoder_by_name(name):
+        return None
+
+    @staticmethod
+    def avcodec_alloc_context3(codec):
+        return {}
+
+    @staticmethod
+    def avcodec_open2(context, codec, params):
         return -1
 
 
@@ -15,27 +28,33 @@ class MockLavf:
     def url_feof(self, pb):
         return False if self.faulty else True
 
-    def av_read_frame(self, ctx, pkt):
+    @staticmethod
+    def av_read_frame(ctx, pkt):
         return -1
+
+
+class MockCFFI:
+    def __init__(self):
+        self.NULL = None
 
 
 class MockFF:
     def __init__(self, faulty):
         self.lavf = MockLavf(faulty)
         self.lavc = MockLavc()
-
-
-class MockPacket:
-    from contextlib import contextmanager
-    @contextmanager
-    def raw_pkt(self):
-        yield {}
+        self.ffi = MockCFFI()
 
 
 class MockAVFormatContext:
     def __init__(self):
         self.pb = None
 
+
+class MockAVCodecContext:
+    def __init__(self, codec_type=None, codec=None):
+        self.codec_type = codec_type
+        self.codec = codec
+ 
 
 class MockPlat:
     def __init__(self, impl='CPython', vers=(3,3)):
