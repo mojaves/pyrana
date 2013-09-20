51ad7191eaad4b902e10dda3b1926093842f789a
Francesco Romani <fromani@gmail.com>
2013-09-20 21:21:01 +0200

    Sketch the Audio decoding side, and add the first basic tests.

--- pyrana/audio.py
+++ pyrana/audio.py
@@ -4,8 +4,8 @@ Encoders, Decoders and their support code.
 """
 
 from pyrana.common import to_sample_format
-from pyrana.codec import BaseFrame, BaseDecoder
-import pyrana.errors
+from pyrana.codec import BaseFrame, BaseDecoder, FrameBinder
+from pyrana.errors import ProcessingError, SetupError
 import pyrana.ff
 # the following is just to export to the clients the Enums.
 # pylint: disable=W0611
@@ -16,14 +16,69 @@ INPUT_CODECS = frozenset()
 OUTPUT_CODECS = frozenset()
 
 
-class Frame(BaseFrame):
+def _samples_from_frame(ffh, frame, smpfmt):
+    pass
+
+
+class Samples(object):
     """
-    An Audio frame.
+    Represents the Sample data inside a Frame.
     """
+    def __init__(self):
+        # mostly for documentation purposes, and to make pylint happy.
+        self._ff = None
+        self._res = None
+        self._ppframe = None
+        raise SetupError("Cannot be created directly. Yet.")
+
+    @classmethod
+    def from_cdata(cls, ppframe, res=None):
+        """
+        builds a pyrana Image from a (cffi-wrapped) libav*
+        Frame object. The Picture data itself will still be hold in the
+        Frame object.
+        The libav object must be already initialized and ready to go.
+        WARNING: raw access. Use with care.
+        """
+        ffh = pyrana.ff.get_handle()
+        samples = object.__new__(cls)
+        samples._ff = ffh
+        samples._res = res
+        samples._ppframe = ppframe
+        return samples
+
     def __repr__(self):
-        return "Frame(sfmt=%i, samples=%i, rate=%i, chans=%i, bps=%i)" % (
-                self.sample_format, self.num_samples,
-                self.sample_rate, self.channels, self.bps)
+        return "Samples(sfmt=%i, samples=%i," \
+               " rate=%i, chans=%i, bps=%i, shared=%s)" \
+               % (self.sample_format, self.num_samples,
+                  self.sample_rate, self.channels, self.bps,
+                  self.is_shared)
+
+    def __del__(self):
+        if not self.is_shared:
+            self._ff.lavc.avcodec_free_frame(self._ppframe)
+
+    def __len__(self):
+        return self.bps * self.num_samples * self.channels
+
+    def __bytes__(self):
+        frm = self._ppframe[0]
+        samples = bytearray(len(self))
+        return bytes(samples)
+
+    @property
+    def is_shared(self):
+        """
+        Is the underlying C-Frame shared with the parent py-Frame?
+        """
+        return self._res is None
+
+    def convert(self, smpfmt):
+        """
+        convert the Image data in a new PixelFormat.
+        returns a brand new, independent Image.
+        """
+        return _samples_from_frame(self._ff, self._ppframe[0], smpfmt)
 
     @property
     def sample_format(self):
@@ -31,35 +86,61 @@ class Frame(BaseFrame):
         Frame sample format. Expected to be always equal
         to the stream sample format.
         """
-        return to_sample_format(self._frame.format)
+        frm = self._ppframe[0]
+        return to_sample_format(frm.format)
 
     @property
     def num_samples(self):
         """
         The number of audio samples (per channel) described by this frame.
         """
-        return self._frame.nb_samples
+        frm = self._ppframe[0]
+        return frm.nb_samples
 
     @property
     def sample_rate(self):
         """
         Sample rate of the audio data.
         """
-        return self._ff.lavc.av_frame_get_sample_rate(self._frame)
+        frm = self._ppframe[0]
+        return self._ff.lavc.av_frame_get_sample_rate(frm)
 
     @property
     def channels(self):
         """
         The number of audio channels, only used for audio.
         """
-        return self._ff.lavc.av_frame_get_channels(self._frame)
+        frm = self._ppframe[0]
+        return self._ff.lavc.av_frame_get_channels(frm)
 
     @property
     def bps(self):
         """
         Bytes per sample.
         """
-        return self._ff.lavu.av_get_bytes_per_sample(self._frame.format)
+        frm = self._ppframe[0]
+        return self._ff.lavu.av_get_bytes_per_sample(frm.format)
+
+
+class Frame(BaseFrame):
+    """
+    An Audio frame.
+    """
+    def __repr__(self):
+        base = super(Frame, self).__repr__()
+        # FIXME
+        return "%s)" \
+                    % (base[:-1])  # FIXME
+
+    def samples(self, smpfmt=None):
+        """
+        Returns a new Image object which provides access to the
+        Picture (thus the pixel as bytes()) data.
+        """
+        if smpfmt is None:  # native data, no conversion
+            return Samples.from_cdata(self._ppframe)
+        return _samples_from_frame(self._ff, self._ppframe[0], smpfmt)
+
 
 
 def _wire_dec(dec):
--- tests/test_decoder.py
+++ tests/test_decoder.py
@@ -179,5 +179,35 @@ class TestImage(unittest.TestCase):
         assert(ffh.sws.scale_done == 1)
 
 
+class TestSamples(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        pyrana.setup()
+
+    def test_cannot_create_samples(self):
+        with self.assertRaises(pyrana.errors.SetupError):
+            img = pyrana.audio.Samples()
+
+    def test_create_synth1(self):
+        ffh = pyrana.ff.get_handle()
+        ppframe = pyrana.codec._new_av_frame_pp(ffh)
+        smp = pyrana.audio.Samples.from_cdata(ppframe)
+        assert(smp.is_shared)
+        ffh.lavc.avcodec_free_frame(ppframe)
+
+    # FIXME: bulky. Also depends on decoder.
+    def test_create_from_live_frame(self):
+        with open(BBB_SAMPLE, 'rb') as f:
+            dmx = pyrana.formats.Demuxer(f)
+            dec = dmx.open_decoder(1)
+            frames = dec.decode(dmx.stream(1))
+            assert(frames)
+            smp = frames[0].samples()
+            assert(smp)
+            assert(repr(smp))
+#            assert(len(smp))
+            assert(smp.is_shared)
+
+
 if __name__ == "__main__":
     unittest.main()
