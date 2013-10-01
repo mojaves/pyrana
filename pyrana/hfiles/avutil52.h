7ee25f993603234e70836bae1166f9d394dacfd7
Francesco Romani <fromani@gmail.com>
2013-10-01 23:01:39 +0200

    export more functions needed by the audio resampling.

--- pyrana/hfiles/avutil52.h
+++ pyrana/hfiles/avutil52.h
@@ -12,6 +12,9 @@ const char *av_get_media_type_string(enum AVMediaType media_type);
 int av_opt_get_int(void *obj, const char *name, int search_flags, int64_t *out_val);
 int av_opt_get_double(void *obj, const char *name, int search_flags, double *out_val);
 
+int av_opt_set_int(void *obj, const char *name, int64_t val, int search_flags);
+int av_opt_set_double(void *obj, const char *name, double val, int search_flags);
+
 typedef struct AVPixFmtDescriptor {
    const char *name;
    uint8_t nb_components;
@@ -31,3 +34,11 @@ int av_image_get_buffer_size(enum AVPixelFormat pix_fmt,
 
 int av_image_alloc(uint8_t *pointers[4], int linesizes[4],
                    int w, int h, enum AVPixelFormat pix_fmt, int align);
+
+/* mathematics.h */
+int64_t av_rescale(int64_t a, int64_t b, int64_t c);
+int64_t av_rescale_rnd(int64_t a, int64_t b, int64_t c, enum AVRounding);
+
+/* channel_layout.h */
+int av_get_channel_layout_nb_channels(uint64_t channel_layout);
+
