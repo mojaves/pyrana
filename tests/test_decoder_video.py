5deaadef2b2d9074acd957d6e2d405572a8210c5
Francesco Romani <fromani@gmail.com>
2013-10-02 21:00:49 +0200

    restore the full video module coverage.

--- tests/test_decoder_video.py
+++ tests/test_decoder_video.py
@@ -113,6 +113,42 @@ class TestImage(unittest.TestCase):
         assert(ffh.sws.scale_done == 1)
 
     # FIXME: bulky. Also depends on decoder.
+    def test_plane_get0(self):
+        with open(BBB_SAMPLE, 'rb') as f:
+            dmx = pyrana.formats.Demuxer(f)
+            dec = dmx.open_decoder(0)
+            img, frm = _next_image(dmx, dec)
+            pln = img.plane(0)
+            assert(pln)
+            assert(isinstance(pln, bytes))
+
+    # FIXME: bulky. Also depends on decoder.
+    def test_plane_get3(self):
+        with open(BBB_SAMPLE, 'rb') as f:
+            dmx = pyrana.formats.Demuxer(f)
+            dec = dmx.open_decoder(0)
+            img, frm = _next_image(dmx, dec)
+            plns = [ img.plane(0),
+                     img.plane(1),
+                     img.plane(2) ]
+            for pln in plns:
+                assert(pln)
+                assert(isinstance(pln, bytes))
+                assert(len(pln) <= len(plns[0]))
+
+    # FIXME: bulky. Also depends on decoder.
+    def test_planes_vs_bytes(self):
+        with open(BBB_SAMPLE, 'rb') as f:
+            dmx = pyrana.formats.Demuxer(f)
+            dec = dmx.open_decoder(0)
+            img, frm = _next_image(dmx, dec)
+            plns = img.plane(0) + img.plane(1) + img.plane(2)
+            data = bytes(img)
+            assert(len(img) == len(plns))
+            assert(len(img) == len(data))
+            assert(data == plns)
+
+    # FIXME: bulky. Also depends on decoder.
     def test_plane_bad1(self):
         with open(BBB_SAMPLE, 'rb') as f:
             dmx = pyrana.formats.Demuxer(f)
@@ -129,6 +165,35 @@ class TestImage(unittest.TestCase):
             img, frm = _next_image(dmx, dec)
             with self.assertRaises(pyrana.errors.ProcessingError):
                 pln = img.plane(10)
+
+    # FIXME: bulky. Also depends on decoder.
+    def test_video_frame_has_not_samples(self):
+        with open(BBB_SAMPLE, 'rb') as f:
+            dmx = pyrana.formats.Demuxer(f)
+            dec = dmx.open_decoder(0)
+            frm = dec.decode(dmx.stream(0))
+            with self.assertRaises(AttributeError):
+                smp = frm.samples()
+
+
+class TestPlaneCopy(unittest.TestCase):
+    def test__plane_copy_bad_src_linesize(self):
+        dst = bytearray(16)
+        src = b'a' * 16
+        with self.assertRaises(pyrana.errors.ProcessingError):
+            num = pyrana.video._plane_copy(dst, src, 15, 16, 16, 1)
+
+    def test__plane_copy_bad_src_linesize(self):
+        dst = bytearray(16)
+        src = b'a' * 16
+        with self.assertRaises(pyrana.errors.ProcessingError):
+            num = pyrana.video._plane_copy(dst, src, 16, 15, 16, 1)
+
+    def test__plane_copy(self):
+        dst = bytearray(16)
+        src = b'a' * 16
+        num = pyrana.video._plane_copy(dst, src, 16, 16, 16, 1)
+        assert(dst == src)
 
 
 if __name__ == "__main__":
