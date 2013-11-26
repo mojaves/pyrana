85a13d706a22cc98d70d41c9ffb41507e018471c
Francesco Romani <fromani@gmail.com>
2013-11-26 08:57:38 +0100

    restore 100% coverage
    and a bit of cleanup (and yet another squashed commit)

--- tests/test_demuxer_seek.py
+++ tests/test_demuxer_seek.py
@@ -13,33 +13,52 @@ import unittest
 BBB_SAMPLE = os.path.join('tests', 'data', 'bbb_sample.ogg')
 
 
-class TestDemuxerSeel(unittest.TestCase):
-    @unittest.expectedFailure
-    def test_seek_video_frameno(self):
+class TestDemuxerSeek(unittest.TestCase):
+    def test_seek_video_frameno_not_ready(self):
+        with open(BBB_SAMPLE, 'rb') as fin:
+            dmx = pyrana.formats.Demuxer(fin, delay_open=True)
+            with self.assertRaises(pyrana.errors.ProcessingError):
+                dmx.seek_frame(0, 3)
+
+    def test_seek_video_ts_not_ready(self):
+        with open(BBB_SAMPLE, 'rb') as fin:
+            dmx = pyrana.formats.Demuxer(fin, delay_open=True)
+            with self.assertRaises(pyrana.errors.ProcessingError):
+                dmx.seek_ts(0, 100)
+
+    def read_pkt_at_ts(self, sid, ts=100):
         with open(BBB_SAMPLE, 'rb') as fin:
             dmx = pyrana.formats.Demuxer(fin)
-            dmx.seek_frame(0, 3)
-            pkt = dmx.read_frame(0)
+            dmx.seek_ts(sid, ts)
+            pkt = dmx.read_frame(sid)
             assert(pkt)
             assert(len(pkt))
+            return dmx
+
+    def read_pkt_at_frameno(self, sid, frameno=3):
+        with open(BBB_SAMPLE, 'rb') as fin:
+            dmx = pyrana.formats.Demuxer(fin)
+            dmx.seek_frame(sid, frameno)
+            pkt = dmx.read_frame(sid)
+            assert(pkt)
+            assert(len(pkt))
+            return dmx
+
+    @unittest.expectedFailure
+    def test_seek_video_frameno(self):
+        self.read_pkt_at_frameno(0)
 
     @unittest.expectedFailure
     def test_seek_audio_frameno(self):
-        with open(BBB_SAMPLE, 'rb') as fin:
-            dmx = pyrana.formats.Demuxer(fin)
-            dmx.seek_frame(1, 3)
-            pkt = dmx.read_frame(1)
-            assert(pkt)
-            assert(len(pkt))
+        self.read_pkt_at_frameno(1)
 
     @unittest.expectedFailure
     def test_seek_video_ts(self):
-        with open(BBB_SAMPLE, 'rb') as fin:
-            dmx = pyrana.formats.Demuxer(fin)
-            dmx.seek_ts(0, 100)
-            pkt = dmx.read_frame(0)
-            assert(pkt)
-            assert(len(pkt))
+        self.read_pkt_at_ts(0)
+
+    @unittest.expectedFailure
+    def test_seek_audio_ts(self):
+        self.read_pkt_at_ts(1)
 
 
 if __name__ == "__main__":
