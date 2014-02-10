3461452f58a39ac1d8d9a4062b4faf03b707c5fc
Francesco Romani <fromani@gmail.com>
2014-02-10 21:45:08 +0100

    more Muxer tests

--- tests/test_muxer.py
+++ tests/test_muxer.py
@@ -18,12 +18,13 @@ from pyrana.audio import SampleFormat, ChannelLayout
 
 
 
-class TestDemuxer(unittest.TestCase):
+class TestMuxer(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         pyrana.setup()
 
     def setUp(self):
+        self.pixfmt = pyrana.video.PixelFormat.AV_PIX_FMT_YUVJ420P
         self.vparams = {
             'bit_rate': 1000,
             'width': 352,
@@ -71,12 +72,63 @@ class TestDemuxer(unittest.TestCase):
             mux = pyrana.formats.Muxer(f, name='avi')
             mux.write_header()
 
+    def test_write_trailer_no_streams(self):
+        with self.assertRaises(pyrana.errors.ProcessingError), \
+                io.BytesIO() as f:
+            f.name = 'bio'  # XXX
+            mux = pyrana.formats.Muxer(f, name='avi')
+            mux.write_header()
+
     def test_write_header_only_video(self):
         with io.BytesIO() as f:
             f.name = 'bio'  # XXX
             mux = pyrana.formats.Muxer(f, name='avi')
             venc = mux.open_encoder('mjpeg', self.vparams)
             mux.write_header()
+            assert f.tell() > 0
+
+    def test_write_trailer_only_video(self):
+        with io.BytesIO() as f:
+            f.name = 'bio'  # XXX
+            mux = pyrana.formats.Muxer(f, name='avi')
+            venc = mux.open_encoder('mjpeg', self.vparams)
+            mux.write_header()
+            mux.write_trailer()
+            assert f.tell() > 0
+
+    def test_write_trailer_requires_header(self):
+        with io.BytesIO() as f:
+            f.name = 'bio'  # XXX
+            mux = pyrana.formats.Muxer(f, name='avi')
+            venc = mux.open_encoder('mjpeg', self.vparams)
+            with self.assertRaises(pyrana.errors.ProcessingError):
+                mux.write_trailer()
+
+    def test_write_trailer_only_video_twice_fails(self):
+        with io.BytesIO() as f:
+            f.name = 'bio'  # XXX
+            mux = pyrana.formats.Muxer(f, name='avi')
+            venc = mux.open_encoder('mjpeg', self.vparams)
+            mux.write_header()
+            mux.write_trailer()
+            with self.assertRaises(pyrana.errors.ProcessingError):
+                mux.write_trailer()
+
+    def test_write_frame_video(self):
+        with io.BytesIO() as f:
+            f.name = 'bio'  # XXX
+            mux = pyrana.formats.Muxer(f, name='avi')
+            venc = mux.open_encoder('mjpeg', self.vparams)
+            mux.write_header()
+            pixfmt = pyrana.video.PixelFormat.AV_PIX_FMT_YUV420P
+            vfrm = pyrana.video.Frame(self.vparams['width'],
+                                     self.vparams['height'],
+                                     pixfmt)
+            pyrana.video.fill_yuv420p(vfrm, 0)
+            # hack, do no try this at home
+            vfrm.cdata.format = self.pixfmt
+            pkt = venc.encode(vfrm)
+            mux.write_frame(pkt)
             assert f.tell() > 0
 
 
